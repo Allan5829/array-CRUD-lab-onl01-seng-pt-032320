83f7b206c1348d016f259530f54fa9cb67a41566
Allan Farca <allan.farca@gmail.com>
2020-03-28 23:28:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,6 +10,7 @@ array = ["wow", "I", "am", "really", "learning"]
 element = "arrays!"
 
 def add_element_to_end_of_array(array, element)
+  array = ["wow", "I", "am", "really", "learning"]
   element = "arrays!"
   array << element
   puts array.inspect
